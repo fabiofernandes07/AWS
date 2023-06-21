47d0052a7716f609f3f7bc06df6c89fc5abc4a99
Fábio Fernandes <fernandesfabio7070@gmail.com>
2023-06-21 10:59:18 -0300

    Add files via upload

---
+++ Terraform/CriandoArquivos/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+  access_key = "*************"
+  secret_key = "*************"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-03d5c68bab01f3496"
+  instance_type = "t2.micro"
+  key_name = "fabio-alura"
+  user_data = <<-EOF
+                 #!/bin/bash
+                 cd /home/ubuntu
+                 echo "<h1>Feito com terra form</h1>" > index.html
+                 nohups busybops httpd -f -p 8080 &
+                 EOF
+  tags = {
+    Name = "TesteAWS"
+  }
+}
+
